e9aca97795aaaef689081a37aac49fbb26239424
Thomas Frey <thomas.frey@code.berlin>
2021-11-05 13:02:47 +0100

    Now plays corresponding Key Sound to Button.
    Modified the function to detect what button (Xylophone key) was pressed, then which key to play.

--- ViewController.swift
+++ ViewController.swift
@@ -4,33 +4,29 @@
 //
 //  Created by Angela Yu on 28/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
-//
+//  Exercised on by Thomas Frey
 
 import UIKit
-//New API to play sound
 import AVFoundation
 
 class ViewController: UIViewController {
     
-    //Audio player Instance
     var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    //button link, function being called
+    
+    //Button link to InterfaceBuilder. Func w/ parameters to play sound
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound()
+        playSound(soundName: sender.currentTitle!)
     }
     
-    func playSound() {
-        //locate audio file stored elsewhere
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+    func playSound(soundName: String) {
+        //No hardcoded Key sound. Dynamic now cuz of parameter
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
                 
     }
 }
-
-
